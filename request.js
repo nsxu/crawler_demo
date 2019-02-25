9604613330ba3c35ebf54dab8ad183bcb9488638
xuniansheng <xuniansheng@carnoc.com>
2019-02-25 11:04:23 +0800

    init

---
+++ request.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const cheerio = require('cheerio')
+const async = require('async')
+
+function parseHtml(html){
+  var $ = cheerio.load(html)
+  const arr = [];
+  $('ul[class=list1] li').each(function(i, elem) {
+    arr[i] = $(this).text();
+  });
+  return arr
+}
+request('http://127.0.0.1/test/', function (error, response, body) {
+  if(response.statusCode === 200){
+    var html = parseHtml(body)
+    console.log(html)
+  }
+});
